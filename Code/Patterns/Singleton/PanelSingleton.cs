28fce3e1ae9254005b8079cb2f9f5b30454e08ea
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-28 23:42:03 +0300

    Add WackyLib.Utils, Singleton/PanelSingleton

---
+++ Code/Patterns/Singleton/PanelSingleton.cs
@@ -0,0 +1,49 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace WackyLib.Utils;
+
+public abstract class PanelSingleton<T> : PanelComponent, IHotloadManaged where T : PanelSingleton<T>
+{
+#nullable enable
+#pragma warning disable SB3000
+	// ReSharper disable once MemberCanBePrivate.Global
+	public static T? Instance { get; private set; }
+#pragma warning restore SB3000
+
+	protected override void OnAwake()
+	{
+		if ( Instance.IsValid() )
+		{
+			Log.Warning( $"Singleton tried to initialize another {typeof(T)}!" );
+			Destroy();
+			return;
+		}
+
+		if ( Active )
+		{
+			Instance = (T)this;
+		}
+	}
+
+	void IHotloadManaged.Destroyed( Dictionary<string, object> state )
+	{
+		state["IsActive"] = Instance == this;
+	}
+
+	void IHotloadManaged.Created( IReadOnlyDictionary<string, object> state )
+	{
+		if ( state.GetValueOrDefault( "IsActive" ) is true )
+		{
+			Instance = (T)this;
+		}
+	}
+
+	protected override void OnDestroy()
+	{
+		if ( Instance == this )
+		{
+			Instance = null;
+		}
+	}
+}
