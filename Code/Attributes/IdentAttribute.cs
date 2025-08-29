855244d5085f1fa81355f741c085e6c58053986a
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-29 16:22:22 +0300

    Add WackyLib.Attributes (Ident, LocalizedName)

---
+++ Code/Attributes/IdentAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WackyLib.Attributes;
+
+[AttributeUsage( AttributeTargets.Field )]
+public class IdentAttribute( string ident ) : Attribute
+{
+	public string Value { get; private set; } = ident;
+}
---
+++ Code/Attributes/LocalizedNameAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WackyLib.Attributes;
+
+[AttributeUsage( AttributeTargets.Field | AttributeTargets.Property )]
+public class LocalizedNameAttribute( string key ) : Attribute
+{
+	public string Value { get; private set; } = key;
+}
